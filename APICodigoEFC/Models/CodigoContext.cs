e6bb64c23d9ed51a478f30aa0cbf718d061d3d00
Hugo Torrico <torricomarquezh@gmail.com>
2024-07-08 20:27:46 -0500

    Agregar archivos de proyecto.

---
+++ APICodigoEFC/Models/CodigoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace APICodigoEFC.Models
+{
+    public class CodigoContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+
+        public CodigoContext(DbContextOptions<CodigoContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ APICodigoEFC/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace APICodigoEFC.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string DocumentNumber { get; set; }
+    }
+}
