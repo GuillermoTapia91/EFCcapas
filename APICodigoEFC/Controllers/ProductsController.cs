a10572c300e5ad4f61058435f3ca9adfb1f4b8c8
Hugo Torrico <torricomarquezh@gmail.com>
2024-07-12 20:12:37 -0500

    CRUD Products

---
+++ APICodigoEFC/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using APICodigoEFC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICodigoEFC.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly CodigoContext _context;
+
+        public ProductsController(CodigoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Product> GetByFilters(string? name)
+        {
+            IQueryable<Product> query = _context.Products.Where(x => x.IsActive);
+
+            if (!string.IsNullOrEmpty(name))
+                query = query.Where(x => x.Name.Contains(name));
+
+            return query.ToList();
+        }
+
+        [HttpPost]
+        public void Insert([FromBody] Product Product)
+        {
+            _context.Products.Add(Product);
+            _context.SaveChanges();
+        }
+        [HttpPut]
+        public void Update([FromBody] Product Product)
+        {
+            _context.Entry(Product).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var Product = _context.Products.Find(id);
+            Product.IsActive = false;
+            _context.Entry(Product).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
--- APICodigoEFC/Models/Customer.cs
+++ APICodigoEFC/Models/Customer.cs
@@ -5,8 +5,7 @@
         public int CustomerID { get; set; }
         public string Name { get; set; }
         public string DocumentNumber { get; set; }
-        public string DocumentType { get; set; }
-
+        public string DocumentType { get; set; }        
         public  bool IsActive { get; set; }
     }
 }
