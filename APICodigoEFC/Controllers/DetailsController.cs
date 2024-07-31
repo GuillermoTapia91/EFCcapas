2952fc77069c7d938d3e37b8127e0908c7e11f03
GuillermoTapia91 <guillermo.tapia1991@gmail.com>
2024-07-31 18:37:30 -0500

    DETAILES & INVOICES

--- APICodigoEFC/Controllers/DetailsController.cs
+++ APICodigoEFC/Controllers/DetailsController.cs
@@ -1,11 +1,12 @@
-﻿using APICodigoEFC.Context;
-using APICodigoEFC.Models;
+﻿using Infraestructure.Context;
+using Domain.Models;
 using APICodigoEFC.Response;
 using APICodigoEFC.Utility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Services.Services;
 
 namespace APICodigoEFC.Controllers
 {
@@ -14,47 +15,44 @@ namespace APICodigoEFC.Controllers
     public class DetailsController : ControllerBase
     {
         private readonly CodigoContext _context;
+        private DetailsService _service;
 
         public DetailsController(CodigoContext context)
         {
             _context = context;
+            _service = new DetailsService(_context);
         }
 
-        [HttpPost]
-        public void Insert([FromBody] Detail detail)
+        [HttpGet]
+        public List<Detail> GetByFilters(string? customerName, string? invoiceNumber)
         {
-            _context.Details.Add(detail);
-            _context.SaveChanges();
+            var details =_service.GetByFilters(customerName, invoiceNumber);
+
+
+            return details;
         }
 
         [HttpGet]
         public List<Detail> Get()
         {
-            IQueryable<Detail> query = _context.Details
-                .Include(x => x.Product)
-                .Include(x => x.Invoice).ThenInclude(y => y.Customer)
-                .Where(x => x.IsActive);
+            var details = _service.Get();
 
 
-            return query.ToList();
+            return details;
         }
-        //Listar todos los detalles y buscar por nombre de cliente.
-        [HttpGet]
-        public List<Detail> GetByFilters(string? customerName, string? invoiceNumber)
+
+        [HttpPost]
+        public void Insert([FromBody] Detail detail)
         {
-            IQueryable<Detail> query = _context.Details
-               .Include(x => x.Product)
-               .Include(x => x.Invoice).ThenInclude(y => y.Customer)
-               .Where(x => x.IsActive);
-
-            if (!string.IsNullOrEmpty(customerName))
-                query = query.Where(x => x.Invoice.Customer.Name.Contains(customerName));
-            if (!string.IsNullOrEmpty(invoiceNumber))
-                query = query.Where(x => x.Invoice.Number.Contains(invoiceNumber));
-
-
-            return query.ToList();
+            _service.Insert(detail);
         }
+
+        /*
+        
+
+        
+        //Listar todos los detalles y buscar por nombre de cliente.
+        
 
 
         [HttpGet]
@@ -112,5 +110,6 @@ namespace APICodigoEFC.Controllers
 
             return response;
         }
+        */
     }
 }
